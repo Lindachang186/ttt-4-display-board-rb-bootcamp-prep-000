83a1f88e8f08a1f5eb718fe137569aaef4104a9f
Linda Chang <lindachang186@gmail.com>
2018-08-15 14:21:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
+def display_board(array)
   dividers = "-----------"
   vertical_dividers = "   |   |   "
   puts vertical_dividers
@@ -10,4 +10,6 @@ def display_board(board)
   puts vertical_dividers
 end
 board = [" "," "," "," "," "," "," "," "," "]
+print display_board(board)
+board[5] = "X"
 print display_board(board)
